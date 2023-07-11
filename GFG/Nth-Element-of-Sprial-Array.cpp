7428a4cd60d38c656992a694cd6522e0f8e6f124
Rahul Kumar S <86195461+HLrahul@users.noreply.github.com>
2023-07-11 06:43:23 +0000

    added ˋGFG solvedˋ problem

---
+++ GFG/Nth-Element-of-Sprial-Array.cpp
@@ -0,0 +1,88 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+#define MAX 1000
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    /*You are required to complete this method*/
+    int findK(int a[MAX][MAX],int n,int m,int k)
+    {
+ 		int count = 0;
+ 		
+ 		int top = 0, bottom = n-1, left = 0, right = m-1;
+ 		while(bottom >= top && right >= left) {
+ 		    
+ 		    for(int i = left; i <= right; i++) {
+ 		        count++;
+ 		        if(count == k) return a[top][i];
+ 		    }
+ 		    top++;
+ 		    
+ 		    for(int i = top; i <= bottom; i++) {
+ 		        count ++;
+ 		        if(count == k) return a[i][right];
+ 		    }
+ 		    right--;
+ 		    
+ 		    
+ 		    if(top <= bottom) {
+ 		    
+ 		    for(int i = right; i >= left; i--) {
+ 		        count++;
+ 		        if(count == k) return a[bottom][i];
+ 		    }
+ 		    bottom--;
+ 		    
+ 		    }
+ 		    
+ 		    if(left <= right) {
+ 		    
+ 		    for(int i = bottom; i >= top; i--) {
+ 		        count++;
+ 		        if(count == k) return a[i][left];
+ 		    }
+ 		    left++;
+ 		    
+ 		    }
+ 		    
+ 		}
+ 		
+ 		return 0;
+    }
+};
+
+
+
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int T;
+    cin>>T;
+  
+    while(T--)
+    {
+        int n,m;
+        int k=0;
+        //cin>>k;
+        cin>>n>>m>>k;
+        int a[MAX][MAX];
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                cin>>a[i][j];
+            }
+        }
+        Solution ob;
+        cout<<ob.findK(a,n,m,k)<<endl;
+        
+       
+    }
+}
+// } Driver Code Ends
