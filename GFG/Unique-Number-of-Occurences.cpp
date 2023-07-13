6db1efb583ea7a19fd5d305815fc6288213cc1fe
Rahul Kumar S <86195461+HLrahul@users.noreply.github.com>
2023-07-13 06:52:38 +0000

    added GFG solved problem

---
+++ GFG/Unique-Number-of-Occurences.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+    public:
+    bool isFrequencyUnique(int n, int arr[])
+    {
+        unordered_map<int, int> frequencies;
+        set<int> freqs;
+        
+        for(int i = 0; i < n; i++) frequencies[arr[i]]++;
+        for(auto const& pair: frequencies) {
+            int x = pair.second;
+            if(freqs.count(x) > 0) return false;
+            else freqs.insert(x);
+        }
+        
+        return true;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        Solution ob;
+        bool ans=ob.isFrequencyUnique(n,arr);
+        if(ans)
+            cout<<1<<endl;
+        else
+            cout<<0<<endl;
+    }
+}
+// } Driver Code Ends
