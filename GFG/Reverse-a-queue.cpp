e3f83ddd6db4205daac47e91b09b328712132c4f
Rahul Kumar S <86195461+HLrahul@users.noreply.github.com>
2023-07-16 18:03:15 +0530

    added GFG solved problem

---
+++ GFG/Reverse-a-queue.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//function Template for C++
+
+//Function to reverse the queue.
+class Solution
+{
+    public:
+    queue<int> rev(queue<int> q)
+    {
+        stack<int> stk;
+        queue<int> que;
+        
+        while(!q.empty()) {
+            stk.push(q.front());
+            q.pop();
+        }
+        
+        while(!stk.empty()) {
+            que.push(stk.top());
+            stk.pop();
+        }
+        
+        return que;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int test;
+    cin>>test; 
+    while(test--)
+    {
+    queue<int> q; 
+    int n, var; 
+    cin>>n; 
+    while(n--)
+    {
+        cin>>var; 
+        q.push(var);
+    }
+    Solution ob;
+    queue<int> a=ob.rev(q); 
+    while(!a.empty())
+    {
+        cout<<a.front()<<" ";
+        a.pop();
+    }
+    cout<<endl; 
+    }
+}
+// } Driver Code Ends
