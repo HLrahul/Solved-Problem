d88747870dee8799f788715e40fddd362850f7f8
Rahul Kumar S <86195461+HLrahul@users.noreply.github.com>
2023-07-22 16:28:33 +0530

    added GFG solved problem

---
+++ GFG/Remove-duplicates-from-Unsorted-linked-list.cpp
@@ -0,0 +1,105 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+void print(Node *root)
+{
+Node *temp = root;
+while(temp!=NULL)
+{
+cout<<temp->data<<" ";
+temp=temp->next;
+}
+}
+
+
+// } Driver Code Ends
+/*
+The structure of linked list is the following
+
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+
+
+class Solution
+{
+    public:
+    //Function to remove duplicates from unsorted linked list.
+    Node *removeDuplicates( Node *head) {
+        unordered_map<int, int> hashMap;
+        
+        struct Node *newHead = NULL;
+        struct Node *temp = newHead;
+        struct Node *traverse = head;
+        
+        while(traverse != NULL) {
+            int num = traverse -> data;
+            hashMap[num]++;
+            
+            if(hashMap[num] < 2) {
+                if(newHead == NULL) newHead = temp = new Node(num);
+                else {
+                    temp -> next = new Node(num);
+                    temp = temp -> next;
+                }
+            }
+            
+            traverse = traverse -> next;
+        }
+        
+        return newHead;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	
+	while(T--)
+	{
+		int K;
+		cin>>K;
+		struct Node *head = NULL;
+        struct Node *temp = head;
+ 
+		for(int i=0;i<K;i++){
+		int data;
+		cin>>data;
+			if(head==NULL)
+			head=temp=new Node(data);
+			else
+			{
+				temp->next = new Node(data);
+				temp=temp->next;
+			}
+		}
+		
+	    Solution ob;
+		Node *result  = ob.removeDuplicates(head);
+		print(result);
+		cout<<endl;
+		
+	}
+	return 0;
+}
+// } Driver Code Ends
