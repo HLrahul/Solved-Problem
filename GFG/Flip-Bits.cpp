3498bb26b6d64746a8a237bf664e9d4f623b77e3
Rahul Kumar S <86195461+HLrahul@users.noreply.github.com>
2023-08-15 14:17:59 +0530

    added GFG solved problem

---
+++ GFG/Flip-Bits.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+
+class Solution{
+    public:
+    int maxOnes(int a[], int n) {
+        int count = 0;
+        int maxRange = 0;
+        int one = 0;
+        
+        for(int i = 0; i < n; i++) {
+            if(a[i] == 0) count ++;
+            else {
+                count--;
+                one++;
+                
+                if(count < 0) count = 0;
+            }
+            
+            maxRange = max(maxRange, count);
+        }
+        
+        return maxRange + one;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int t; cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int a[n+5];
+        for(int i=0;i<n;i++)
+            cin>>a[i];
+        Solution ob;
+        cout<< ob.maxOnes(a, n) <<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
